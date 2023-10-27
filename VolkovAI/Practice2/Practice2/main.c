7190559b224f07056dbc8654e316fd0e045e5eb0
fleezeex <leshavolkovv@gmail.com>
2023-10-27 14:17:22 +0300

    Bug fixes

--- VolkovAI/Practice2/Practice2/main.c
+++ VolkovAI/Practice2/Practice2/main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main () {
-    int num = 0, current = 0, mode, border1 = 1, border2 = 1000;
+    int num = 0, current = 0, mode, border1 = 1, border2 = 1000, counter = 1;
     char sign;
     printf ("The program has two modes: \n1. The program makes a number; \n2. The user makes a number. \nPlease, select the mode [1/2]: ");
     scanf ("%d", &mode);
@@ -14,12 +14,16 @@ int main () {
         while (current != num) {
             printf ("Please, input your number: ");
             scanf ("%d", &current);
-            if (current > num)
-                printf ("The hidden number is smaller. ");
-            else if (current < num)
-                printf ("The hidden number is bigger. ");
-            else 
-                printf ("You guessed it!");
+            if (current > num) {
+                printf("The hidden number is smaller. ");
+            }
+            else if (current < num) {
+                printf("The hidden number is bigger. ");
+            }
+            else {
+                printf("You guessed it! Number of trying: %d", counter);
+            }
+            counter++;
         }
     }
     if (mode == 2) {
@@ -34,17 +38,17 @@ int main () {
             }
             sign = getchar();
             if (sign == '>') {
-                border1 = current;
-                current = border1 + rand() % (border2 - border1 + 1);
+                border1 = current + 1;
             }
             else if (sign == '<') {
-                border2 = current;
-                current = border1 + rand() % (border2 - border1 + 1);
+                border2 = current - 1;
             }
             else if (sign == '=') {
-                printf("I won!");
+                printf("I won! Number of trying: %d.", counter);
                 break;
             }
+            current = border1 + rand() % (border2 - border1 + 1);
+            counter++;
         }
     }
     return 0;
