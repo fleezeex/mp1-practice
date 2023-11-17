674189c90f205a73bc8daad20efd1d9aa6badb0a
fleezeex <leshavolkovv@gmail.com>
2023-11-17 13:24:44 +0300

    New fill_user

--- VolkovAI/Practice3/Practice3/main.c
+++ VolkovAI/Practice3/Practice3/main.c
@@ -16,7 +16,7 @@ int main () {
     srand ((unsigned int) time (0));
     fill_rand (&(num[0]), n);
     do {
-        fill_user (&(user_num[0]), n);
+        fill_user(&(user_num[0]), n);
         bulls = cowsbulls (&(num[0]), &(user_num[0]), n);
         att++;
     } while (bulls != n);
@@ -27,16 +27,16 @@ int main () {
 int cowsbulls (int *num, int *user_num, int n) {
     int i = 0, j = 0, cows = 0, bulls = 0;
     for (i = 0; i<n; i++) {
-                for (j = 0; j<n; j++) {
-                    if (num [i] == user_num [j]) {
-                        if (i == j) {
-                            bulls += 1;
-                        }
-                        else {
-                            cows += 1;
-                        }
-                    }
+        for (j = 0; j<n; j++) {
+            if (num [i] == user_num [j]) {
+                if (i == j) {
+                    bulls++;
                 }
+                else {
+                    cows++;
+                }
+            }
+        }
     }
     printf ("Cows = %d, bulls = %d.", cows, bulls);
     return bulls;
@@ -60,19 +60,13 @@ void fill_rand (int *num, int n) {
     }
 }
 
-void fill_user (int *user_num, int n) {
-    int current = 0;
-    printf ("\nInput your number: ");
-    scanf ("%d", &current);
-    user_num[0] = current / pow (10, n-1);
-    user_num[n-1] = current % 10;
-    if (current > 100) {
-        user_num[1] = (int) (current / pow (10, n-2)) % 10;
-        if (current > 1000) {
-            user_num[2] = (int) (current / pow (10, n-3)) % 10;
-            if (current > 10000) {
-                user_num[3] = (current / 10) % 10;
-            }
-        }
+void fill_user(int* user_num, int n) {
+    int current = 0, i = 0, x = 0;
+    printf("\nInput your number: ");
+    scanf("%d", &current);
+    for (int i = n - 1; i >= 0; i--) {
+        x = current % 10;
+        user_num[i] = x;
+        current = current / 10;
     }
 }
