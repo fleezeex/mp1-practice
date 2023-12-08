f2a109d78c8fc0d1e9c507f6ed728c460e43d793
fleezeex <leshavolkovv@gmail.com>
2023-12-08 13:53:13 +0300

    Bug fix

--- VolkovAI/Practice4/Practice4/main.c
+++ VolkovAI/Practice4/Practice4/main.c
@@ -3,12 +3,19 @@
 #include <string.h>
 
 #define N 10
-#define N_SYMBOLS 4
+#define N_SYMBOLS 5
+#define MAX_LEN 255
 
-char* barcodes[N] = { "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008", "1009", "1010" };
-char* names[N] = { "NVIDIA GeForce RTX 4090 24Gb", "Intel Core i9-139000", "GIGABYTE Z790 AORUS XTREME X", "Kingston FURY Renegade 2x32GB DDR5", "be quiet! DARK POWER PRO 12 1500W", "Cooler Master Cosmos C700M", "WD Red SN700 4TB", "Cooler Master MasterAir MA612", "Samsung Odyssey Ark G97NC", "Microsoft Windows 11 Pro" };
-int prices[N] = { 237990, 75990, 129990, 35990, 53490, 85490, 54990, 15490, 239990, 20990 };
-int discount[N] = { 5, 20, 10, 15, 5, 10, 7, 8, 10, 50 };
+char* barcodes[N] = { "1001", "1002", "1003", "1004", "1005", "1006", "1007",
+                      "1008", "1009", "1010" };
+char* names[N] = { "NVIDIA GeForce RTX 4090 24Gb", "Intel Core i9-139000",
+                   "GIGABYTE Z790 AORUS XTREME X", "Kingston FURY Renegade 2x32GB DDR5",
+                   "be quiet! DARK POWER PRO 12 1500W", "Cooler Master Cosmos C700M",
+                   "WD Red SN700 4TB", "Cooler Master MasterAir MA612",
+                   "Samsung Odyssey Ark G97NC", "Microsoft Windows 11 Pro" };
+float prices[N] = { 237990, 75990, 129990, 35990, 53490, 85490, 54990, 15490,
+                    239990, 20990 };
+float discount[N] = { 5, 20, 10, 15, 5, 10, 7, 8, 10, 50 };
 int amount[N] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 
@@ -20,11 +27,13 @@ int program();
 
 
 int main() {
-    int num, i;
+    int num, i = -1;
+    setlocale(LC_ALL, "Rus");
     num = program();
     while (1) {
         if (num == 1) {
             i = scanbarcode();
+            add_amount(i);
             num = program();
         }
         else if (num == 2) {
@@ -32,59 +41,61 @@ int main() {
             num = program();
         }
         else if (num == 3) {
-            add_amount(i);
-            num = program();
-        }
-        else if (num == 4) {
             final_amount();
             return 0;
+        }
+        else
+        {
+            printf("Некорректный код операции!\n");
+            num = program();
         }
     }
 }
 
 int program() {
-    setlocale(LC_ALL, "Rus");
     int num;
     printf("-------------------------\n");
-    printf("1. Сканировать штрих-код; \n2. Вывод описания отсканированного наименования; \n3. Добавить данные о товаре в чек; \n4. Сформировать итоговый чек; \n");
+    printf("1. Сканировать штрих-код; \n2. Вывод описания отсканированного наименования; \n3. Сформировать итоговый чек; \n");
     printf("-------------------------\n");
     scanf("%d", &num);
     return num;
 }
 
 int scanbarcode() {
-    char* code[N_SYMBOLS];
+    char code[MAX_LEN];
     int counter = 0, i = 0;
     printf("Введите штрих-код: ");
-    scanf("%s", &code);
+    scanf("%s", code);
     for (i = 0; i < N; i++) {
         if (strcmp(code, barcodes[i]) == 0) {
-            printf("Товар успешно отсканирован!");
+            printf("Товар успешно отсканирован! \n");
             return i;
         }
     }
     printf("Штрих-код не был найден.\n");
+    return -1;
 }
 
 float output(int i, int num) {
     int num1 = i;
     int num0 = num;
-    float endprice = 0;
-    endprice = prices[num1] - (prices[num1] * discount[num1] * 0.01);
+    float endprice = 0.f;
+    endprice = prices[num1] - (prices[num1] * discount[num1] * 0.01f);
     if (num0 == 2) {
-        printf("Товар: %s | Стоимость: %d руб. | Скидка: %d%% | Цена со скидкой: %f руб.\n", names[num1], prices[num1], discount[num1], endprice);
+        printf("Товар: %s | Стоимость: %f руб. | Скидка: %f%% | Цена со скидкой: %f руб.\n",
+               names[num1], prices[num1], discount[num1], endprice);
     }
     else {
-        endprice = prices[num1] * amount[num1] - (prices[num1] * amount[num1] * discount[num1] * 0.01);
-        printf("Товар: %s | Кол-во: %d | Стоимость: %d руб. | Скидка: %d%% | Цена со скидкой: %f руб.\n", names[num1], amount[num1], prices[num1] * amount[num1], discount[num1], endprice);
+        endprice = prices[num1] * amount[num1] - (prices[num1] * amount[num1] * discount[num1] * 0.01f);
+        printf("Товар: %s | Кол-во: %d | Стоимость: %f руб. | Скидка: %f%% | Цена со скидкой: %f руб.\n",
+               names[num1], amount[num1], prices[num1] * amount[num1], discount[num1], endprice);
         return endprice;
     }
     return 0;
 }
 
 void add_amount(int i) {
-    if (i > 0 && i < 11) {
-        int num = 0;
+    if (i >= 0 && i < N + 1) {
         amount[i]++;
         printf("Товар успешно добавлен в чек!\n");
     }
@@ -95,7 +106,7 @@ void add_amount(int i) {
 
 void final_amount() {
     int i = 0, num = 4;
-    float endprice = 0;
+    float endprice = 0.f;
     printf("\n-------- КАССОВЫЙ ЧЕК --------\n");
     for (i = 0; i < N; i++) {
         if (amount[i] > 0) {
